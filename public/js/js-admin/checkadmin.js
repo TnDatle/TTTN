0e919f5b954c5641e9b35f96caa538b784e52ce7
LeTanDat21 <datle211104@gmail.com>
2025-03-10 11:24:35 +0700

    Fix admin

--- public/js/js-admin/checkadmin.js
+++ public/js/js-admin/checkadmin.js
@@ -1,7 +1,7 @@
 // Kiểm tra trạng thái đăng nhập của admin
 function checkAdminLogin() {
     const isLoggedIn = localStorage.getItem('isLoggedIn');
-    
+
     if (isLoggedIn !== 'true') {
         // Nếu admin chưa đăng nhập, chuyển hướng đến trang login
         window.location.href = "../login-admin/Loginadmin.html";
@@ -16,20 +16,26 @@ function checkAdminLogin() {
 // Kiểm tra đăng nhập khi tải trang admin
 window.onload = checkAdminLogin;
 
-
 document.addEventListener('DOMContentLoaded', function() {
     // Hàm xử lý đăng xuất
     function logoutAdmin() {
+        // Hiển thị hộp thoại xác nhận
+        const confirmLogout = confirm("Bạn có chắc chắn muốn đăng xuất không?");
         
-        // Xóa thông tin đăng nhập trong localStorage
-        localStorage.removeItem('isLoggedIn');
-        localStorage.removeItem('adminname');
+        if (confirmLogout) {
+            // Xóa thông tin đăng nhập trong localStorage
+            localStorage.removeItem('isLoggedIn');
+            localStorage.removeItem('adminname');
 
-        // Hiển thị thông báo đăng xuất thành công
-        alert("Đăng xuất thành công!");
+            // Xóa lịch sử để ngăn quay lại trang admin sau khi đăng xuất
+            history.replaceState(null, null, "Loginadmin.html");
 
-        // Chuyển hướng về trang đăng nhập
-        window.location.href = "Loginadmin.html"; // Chuyển hướng về trang đăng nhập
+            // Hiển thị thông báo đăng xuất thành công
+            alert("Đăng xuất thành công!");
+
+            // Chuyển hướng về trang đăng nhập
+            window.location.href = "Loginadmin.html";
+        }
     }
 
     // Lắng nghe sự kiện click vào thẻ <a>
